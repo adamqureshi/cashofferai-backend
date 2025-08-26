8216e27316832f3ecae62c5539a32d61e048ff69
Adam Qureshi <adamqureshi@gmail.com>
2025-08-26 14:55:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-
+const axios = require('axios');  // Add axios here
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
 
 app.get('/', (req, res) => {
     res.json({ 
